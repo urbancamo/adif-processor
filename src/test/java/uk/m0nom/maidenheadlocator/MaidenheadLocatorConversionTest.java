a3e1feda7ed37cb9a5b91d2bc364e54297cf3129
urbancamo <mark@wickensonline.co.uk>
2021-03-24 23:59:00 +0000

    * Maidenhead Locator Conversion start of test cases

--- src/test/java/uk/m0nom/maidenheadlocator/MaidenheadLocatorConversionTest.java
+++ src/test/java/uk/m0nom/maidenheadlocator/MaidenheadLocatorConversionTest.java
@@ -6,9 +6,23 @@ import static junit.framework.TestCase.assertEquals;
 
 public class MaidenheadLocatorConversionTest {
     @Test
-    public void testLocatorToLatLng() {
+    public void testLocatorToLatLng6Char() {
         LatLng ll = MaidenheadLocatorConversion.locatorToLatLng("IO84ni");
-        assertEquals(String.format("%.3f", ll.longitude), "-2.875");
-        assertEquals(String.format("%.5f", ll.latitude), "54.35417");
+        assertEquals("-2.875", String.format("%.3f", ll.longitude));
+        assertEquals("54.354", String.format("%.3f", ll.latitude));
+    }
+
+    @Test
+    public void testLocatorToLatLng10Char1() {
+        LatLng ll = MaidenheadLocatorConversion.locatorToLatLng("IO84mk33mp");
+        assertEquals("-2.971", String.format("%.3f", ll.longitude));
+        assertEquals("54.432", String.format("%.3f", ll.latitude));
+    }
+
+    @Test
+    public void testLocatorToLatLng10Char2() {
+        LatLng ll = MaidenheadLocatorConversion.locatorToLatLng("IO84ld28ss");
+        assertEquals("-3.060", String.format("%.3f", ll.longitude));
+        assertEquals("54.162", String.format("%.3f", ll.latitude));
     }
 }
