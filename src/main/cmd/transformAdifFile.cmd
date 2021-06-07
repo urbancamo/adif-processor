d3cce91c913231f67f3c2822ff323325b0852e30
Mark Wickens <mark.wickens@hansard.com>
2021-06-07 15:24:25 +0100

    Standardize on Zulu-11 for JDK Update CMD files to reference Zulu-11 Pull in latest yaml control files from the Fast Log Entry directory

--- src/main/cmd/transformAdifFile.cmd
+++ src/main/cmd/transformAdifFile.cmd
@@ -1,1 +1,2 @@
-java -cp target\adif-processor-1.0-SNAPSHOT-jar-with-dependencies.jar uk.m0nom.adif3.FileTransformerApp %1 %2
+set APP=C:\Projects\github\adif-processor
+"C:\Program Files\Zulu\zulu-11\java.exe" -cp %APP%\target\adif-processor-1.0-SNAPSHOT-jar-with-dependencies.jar uk.m0nom.adif3.FileTransformerApp %1
