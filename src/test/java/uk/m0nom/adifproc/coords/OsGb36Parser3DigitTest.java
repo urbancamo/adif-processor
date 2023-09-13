dc4c054b909608fb2999f1c654c819050f899962
Mark Wickens <mark@wickensonline.co.uk>
2023-09-13 10:42:41 +0200

    * Fix the 3 digit output as it was selecting the wrong characters * test to ensure fix is working

--- src/test/java/uk/m0nom/adifproc/coords/OsGb36Parser3DigitTest.java
+++ src/test/java/uk/m0nom/adifproc/coords/OsGb36Parser3DigitTest.java
@@ -7,8 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OsGb36Parser3DigitTest {
     @Test
-    public void test() {
+    public void testConvertOsGb36Parse3DigitToCoord() {
         check(" SD261849 ", 54.25479084403316, -3.1358811832581135);
+    }
+
+    @Test
+    public void testCoordTo3DigitOsGb36() {
+        GlobalCoordinates coords = new Maidenhead10CharLocatorParser().parse(LocationSource.UNDEFINED, "IO83PO38BM");
+        String osGb36ThreeDigit = new OsGb36Parser3Digit().format(coords);
+        assertThat(osGb36ThreeDigit).isEqualTo("SD 520 138");
     }
 
     private void check(String input, Double latitude, Double longitude) {
